fbb1bc11fbed45a1e32991af7c39190ed0753a6c
kangyerim <yerimkang521@gmail.com>
2020-12-16 17:43:52 +0900

    paintTodos(), saveTodos()
    1. 인풋에 입력할 때, li태그 생성 - paintTodos()
    2. 생성된 li태그의 내용들 로컬 스토리지에 저장 - saveTodos()

--- moment/todo.js
+++ moment/todo.js
@@ -3,21 +3,47 @@ const todoForm = document.querySelector(".todoForm"),
       todoList = document.querySelector(".todoList");
 
 const TODOS_LOCAL = "todos";
+let toDos = [];
+
+function saveTodos() {
+  localStorage.setItem(TODOS_LOCAL, JSON.stringify(toDos))
+}
 
 function paintTodos(text) {
-  console.log(text)
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "delete";
+  const span = document.createElement("span");
+  span.innerText = text;
+  const newId = toDos.length + 1;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  todoList.appendChild(li);
+
+  const todoObj = {
+    id : newId,
+    text : text
+  }
+  toDos.push(todoObj);
+  saveTodos();
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  const value = todoInput.value;
-  paintTodos(value);
-  value = "";
+  let currentValue = todoInput.value;
+  paintTodos(currentValue);
+  todoInput.value = "";
 }
 
 function loadTodos() {
-  const todos = localStorage.getItem(TODOS_LOCAL);
-  if(todos !== null) {}
+  const loadedToDos = localStorage.getItem(TODOS_LOCAL);
+  if(loadedToDos !== null) {
+    const parsedTodos = JSON.stringify(loadedToDos);
+    parsedTodos.forEach(function(todo) {
+      paintTodos(todo.text)
+    });
+  }
 }
 
 function init() {
