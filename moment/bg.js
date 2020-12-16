31cf45f18db5f2baa0c22c66db92ba0c4ea6f0d6
kangyerim <yerimkang521@gmail.com>
2020-12-16 19:05:09 +0900

    소스이미지 body에 붙임

--- moment/bg.js
+++ moment/bg.js
@@ -2,6 +2,12 @@ const body = document.querySelector("body");
 
 const IMG_NUM = 5;
 
+function paintImage(imgNum) {
+  const img = new Image();
+  img.src = `images/${imgNum + 1}.jpg`;
+  img.classList.add("bgImage");
+  body.prepend(img)
+}
 
 function getRandom() {
   const number = Math.floor(Math.random() * IMG_NUM);
@@ -10,6 +16,7 @@ function getRandom() {
 
 function init() {
   const randomNum = getRandom();
+  paintImage(randomNum);
 }
 
 init();
