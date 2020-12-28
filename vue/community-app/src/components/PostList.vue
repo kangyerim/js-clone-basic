9d7291df4122c19fd2d00f3d2572a60982af5a77
kangyerim <yerimkang521@gmail.com>
2020-12-28 21:37:16 +0900

    community-app 생성 및 게시글 리스트 받아오기

---
+++ vue/community-app/src/components/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <table>
+        <colgroup>
+          <col style="width: 10%;" />
+          <col style="width: 60%;" />
+          <col style="width: 10%;" />
+          <col style="width: 20%;" />
+        </colgroup>
+        <thead>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">Title</th>
+            <th scope="col">Writer</th>
+            <th scope="col">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(post) in posts" :key="post.id">
+            <td scope="col">{{post.id}}</td>
+            <td scope="col">{{post.title}}</td>
+            <td scope="col">{{post.user.name}}</td>
+            <td scope="col">{{post.createdAt}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'post-list',
+  props: {
+    posts: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/community-app/src/pages/PostListPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="post-list-page">
+    <h1>Post List</h1>
+    <post-list :posts="posts"/>
+  </div>
+</template>
+
+<script>
+import api from '@/api';
+import PostList from '@/components/PostList';
+
+export default {
+  name: 'post-list-page',
+  components: { PostList },
+  data: () => ({
+    posts: []
+  }),
+  created() {
+    api.get('/posts')
+    .then(res => this.posts = res.data)
+  }
+}
+</script>
+
+<style>
+
+</style>
