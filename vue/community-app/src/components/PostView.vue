58aab34761181c2e3305bfa84d33b8dda67da744
kangyerim <yerimkang521@gmail.com>
2020-12-29 00:13:43 +0900

    props의 자료형 체크를 위해 validator 추가

---
+++ vue/community-app/src/components/PostView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="post-view">
+      <div>
+        <h1>{{post.title}}</h1>
+        <span>게시판 번호 {{post.id}}</span>
+        <strong>{{post.user.name}} . {{post.createdAt}}</strong>
+      </div>
+      <p>{{post.contents}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'post-view',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+      validator(post) {
+        const isValidPostId = typeof post.id === 'number'
+        const isValidTitle = !!post.title && post.title.length
+        const isValidContents = post.contents && post.contents.length
+        return isValidPostId && isValidTitle && isSecureContext
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- vue/community-app/src/pages/PostViewPage.vue
+++ vue/community-app/src/pages/PostViewPage.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="post-view-page">
-    <div class="post-view">
-      <div>
-        <h1>게시글의 제목이 노출된다</h1>
-        <span>게시판 번호 1</span>
-        <strong>강예림 . 2020-12-28 22:24</strong>
-      </div>
-      <p>해당 영역은 게시글의 콘텐츠 내용</p>
-    </div>
+    <post-view :post="post" />
     <router-link :to="{ name: 'PostListPage' }">목록</router-link>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapState, mapActions } from 'vuex';
+import PostView from '@/components/PostView';
 
 export default {
   name: 'post-view-page',
+  components: {
+    PostView
+  },
   props: {
     postId: {
       type: String,
       required: true,
     }
+  },
+  computed: {
+    ...mapState(['post'])
   },
   methods: {
     ...mapActions(['fetchPost']),
